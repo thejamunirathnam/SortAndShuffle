4f7ec9f7df67458ab2fc7f222f70dc1657b2cd43
thejamunirathnam <thejasree1996@gmail.com>
2024-05-09 21:10:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cardsContainer = document.querySelector('.cards-container');
+    const shuffleBtn = document.getElementById('shuffleBtn');
+    const sortBtn = document.getElementById('sortBtn');
+  
+    
+    function shuffleCards() {
+      for (let i = cardsContainer.children.length; i >= 0; i--) {
+        cardsContainer.appendChild(cardsContainer.children[Math.random() * i | 0]);
+      }
+    }
+    
+    function sortCards() {
+      Array.from(cardsContainer.children)
+        .sort((a, b) => a.textContent.localeCompare(b.textContent))
+        .forEach(card => cardsContainer.appendChild(card));
+    }
+  
+    shuffleBtn.addEventListener('click', shuffleCards);
+    sortBtn.addEventListener('click', sortCards);
+  });
+  
