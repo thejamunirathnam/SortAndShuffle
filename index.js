b8db2ed839bc23f23d4c137a3ce0a3c69aa717bb
thejasree <thejasree1996@gmail.com>
2024-05-13 10:22:29 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const cardsContainer = document.querySelector('.cards-container');
     const shuffleBtn = document.getElementById('shuffleBtn');
     const sortBtn = document.getElementById('sortBtn');
-  
-    
+
+
     function shuffleCards() {
-      for (let i = cardsContainer.children.length; i >= 0; i--) {
-        cardsContainer.appendChild(cardsContainer.children[Math.random() * i | 0]);
-      }
+        for (let i = cardsContainer.children.length; i >= 0; i--) {
+            cardsContainer.appendChild(cardsContainer.children[Math.random() * i | 0]);
+        }
     }
-    
+
     function sortCards() {
-      Array.from(cardsContainer.children)
-        .sort((a, b) => a.textContent.localeCompare(b.textContent))
-        .forEach(card => cardsContainer.appendChild(card));
+        Array.from(cardsContainer.children)
+            .sort((a, b) => a.textContent.localeCompare(b.textContent))
+            .forEach(card => cardsContainer.appendChild(card));
     }
-  
+
     shuffleBtn.addEventListener('click', shuffleCards);
     sortBtn.addEventListener('click', sortCards);
-  });
-  
+});
